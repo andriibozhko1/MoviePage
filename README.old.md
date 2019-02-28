b37fd64f585972bcea1ee49692931c3a247b49d3
Andrii <andriibozhko1@gmail.com>
2019-02-28 16:23:51 +0200

    to gh-pages

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# MoviePage
---
+++ precache-manifest.06dec6ce4c6153d37fa1d3919b3fec7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c1be619bcb9abfdfdc6",
+    "url": "/MoviePage/static/css/main.f65b44b6.chunk.css"
+  },
+  {
+    "revision": "0c1be619bcb9abfdfdc6",
+    "url": "/MoviePage/static/js/main.0c1be619.chunk.js"
+  },
+  {
+    "revision": "d9adc2e36428a8e9f05b",
+    "url": "/MoviePage/static/js/runtime~main.d9adc2e3.js"
+  },
+  {
+    "revision": "573698287b10209aaf23",
+    "url": "/MoviePage/static/js/2.57369828.chunk.js"
+  },
+  {
+    "revision": "e0c4e3e3d88dc811c76fc0bbfb22d81a",
+    "url": "/MoviePage/static/media/down-arrow-white.e0c4e3e3.svg"
+  },
+  {
+    "revision": "4bdcc80c32dd8fee4866b7ffa954ff7c",
+    "url": "/MoviePage/static/media/logo.4bdcc80c.jpg"
+  },
+  {
+    "revision": "bfc1299e2b31c4906b8d6e6d43596b9f",
+    "url": "/MoviePage/index.html"
+  }
+];
